dc68b1413aac58d6c531c23657da0a5b7178c5c5
kristaponis <a.kristaponis@icloud.com>
2021-01-03 22:03:24 +0200

    Added Gin router

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func getHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Labas",
+	})
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", getHandler)
+
+	r.Run()
 }
